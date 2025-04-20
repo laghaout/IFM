b37b402ffbe59adeb7f9fbececa7a1cc9433aa31
Amine <aknary@yahoo.com>
2025-04-20 12:33:53 +0200

    p. 111

--- main.py
+++ main.py
@@ -5,43 +5,79 @@ Created on Mon Apr 14 19:52:40 2025
 @author: amine
 """
 
+from functools import reduce
+import numpy as np
+import pandas as pd
 from pydantic import BaseModel
 from typing import Optional
-import numpy as np
-
 
 class IFM(BaseModel):
-    N: Optional[int] = None
-    psi_photon: Optional[object] = None
-    psi_qubits: Optional[object] = None
+    N: Optional[int] = None                 # Number of qubits
+    psi_photon: Optional[object] = None     # Photonic state
+    psi_qubits: Optional[object] = None     # Qubit states
+    psi: Optional[object] = None            # Overall state
 
     def __call__(self):
-        
-        self.validate()
-        print(self.N)
-    
-    def validate(self):
-        if self.N is None:
+
+        # If the number of qubits is specified, use an equal distribution of
+        # the photonic and qubit modes. This shall serve as the default.
+        if isinstance(self.N, int):
+            self.psi_photon = np.hstack([np.zeros(1), np.ones(self.N)])
+            self.psi_qubits = np.ones([self.N, 2])
+        else:
             self.N = len(self.psi_photon)
-        assert self.N == len(self.psi_photon)
-        assert self.psi_qubits.shape[0] == self.N
-        assert self.psi_qubits.shape[1] == 2
+
+        self.validate()
+
+        print("psi_photon =\n", self.psi_photon)
+        print("psi_qubits =\n", self.psi_qubits)
+
+        self.psi_qubits = reduce(np.kron, self.psi_qubits)
+        self.psi = np.kron(self.psi_qubits, self.psi_photon)
         
+        max_k = 2**(self.N)
+        index_tuples = [(k, n) for k in range(max_k) 
+                        for n in range(self.N + 1)]
+        index = pd.MultiIndex.from_tuples(index_tuples, names=["k", "n"])
+        self.psi = pd.DataFrame(
+            index=index, data={"amplitude": self.psi})
+
+    def validate(self):
+
+        assert self.N == len(self.psi_photon) - 1 == self.psi_qubits.shape[0]
+        assert self.psi_qubits.shape[1] == 2
+
         # Normalize the photonic state.
         norm_photon = np.sqrt(self.psi_photon @ np.conjugate(self.psi_photon))
-        self.psi_photon = self.psi_photon / norm_photon
-        norm_photon = np.sqrt(self.psi_photon @ np.conjugate(self.psi_photon))
-        assert np.isclose(norm_photon, 1)
-        
+        if not np.isclose(norm_photon, 1):
+            self.psi_photon = self.psi_photon / norm_photon
+            norm_photon = np.sqrt(
+                self.psi_photon @ np.conjugate(self.psi_photon))
+            assert np.isclose(norm_photon, 1)
+            print("Normalizing the photonic state.")
+
         # Normalize the qubits.
-        # norm_qubits = np.sqrt(self.psi_qubits * np.conjugate(self.psi_qubits))
-        # self.psi_qubits = self.psi_qubits / norm_qubits        
-        # norm_qubits = np.sqrt(self.psi_qubits * np.conjugate(self.psi_qubits))        
-        # assert np.isclose(norm_qubits, 1)
-    
-A = IFM(psi_photon=np.ones(3),
-        psi_qubits=np.random.rand(3, 2))
+        norm_qubits = self.psi_qubits * np.conjugate(self.psi_qubits)
+        norm_qubits = np.sqrt(norm_qubits.sum(axis=1)).reshape(-1, 1)
+        if not np.all(
+                np.isclose(norm_qubits, np.ones(self.psi_qubits.shape[0]))):
+            self.psi_qubits = self.psi_qubits / norm_qubits
+            norm_qubits = self.psi_qubits * np.conjugate(self.psi_qubits)
+            norm_qubits = np.sqrt(norm_qubits.sum(axis=1))
+            assert np.all(
+                np.isclose(norm_qubits, np.ones(self.psi_qubits.shape[0])))
+            print("Normalizing the qubits states.")
+
+    @staticmethod
+    def symmetric_BS(N):
+        phi = np.exp(1j*np.pi/N)
+        return phi
+
+
+A = IFM(N=3)
 A()
+
+# for k in 
 
 # def foo(i, n, N, phi, P, Z):
 #     """
