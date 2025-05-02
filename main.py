59c731976b1b6972c096ade666e542bca8733cde
Amine <aknary@yahoo.com>
2025-05-02 16:29:17 +0200

    Added the beam splitter

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import pandas as pd
 from pydantic import BaseModel
 from typing import Optional
 
+
 class IFM(BaseModel):
     N: Optional[int] = None                 # Number of qubits
     psi_photon: Optional[object] = None     # Photonic state
@@ -20,11 +21,12 @@ class IFM(BaseModel):
     def __call__(self):
 
         # If the number of qubits is specified, use an equal distribution of
-        # the photonic and qubit modes. This shall serve as the default.
+        # the photonic and qubit modes. This shall be the default.
         if isinstance(self.N, int):
             self.psi_photon = np.hstack([np.zeros(1), np.ones(self.N)])
             self.psi_qubits = np.ones([self.N, 2])
         else:
+            assert len(self.psi_photon) == self.psi_qubits.shape[0]
             self.N = len(self.psi_photon)
 
         self.validate()
@@ -34,15 +36,24 @@ class IFM(BaseModel):
 
         self.psi_qubits = reduce(np.kron, self.psi_qubits)
         self.psi = np.kron(self.psi_qubits, self.psi_photon)
-        
+
         max_k = 2**(self.N)
-        index_tuples = [(k, n) for k in range(max_k) 
+        index_tuples = [(k, n) for k in range(max_k)
                         for n in range(self.N + 1)]
         index = pd.MultiIndex.from_tuples(index_tuples, names=["k", "n"])
         self.psi = pd.DataFrame(
             index=index, data={"amplitude": self.psi})
+        
+        self.psi['ket'] = self.psi.index.map(
+            lambda k: self.int_to_binary(k[0], self.N)+'⊗'+str(k[1]))
 
-    def validate(self):
+        self.interact()
+
+        self.beam_split()
+        
+        self.measure()
+
+    def validate(self, normalize=True):
 
         assert self.N == len(self.psi_photon) - 1 == self.psi_qubits.shape[0]
         assert self.psi_qubits.shape[1] == 2
@@ -56,42 +67,63 @@ class IFM(BaseModel):
             assert np.isclose(norm_photon, 1)
             print("Normalizing the photonic state.")
 
-        # Normalize the qubits.
-        norm_qubits = self.psi_qubits * np.conjugate(self.psi_qubits)
-        norm_qubits = np.sqrt(norm_qubits.sum(axis=1)).reshape(-1, 1)
-        if not np.all(
-                np.isclose(norm_qubits, np.ones(self.psi_qubits.shape[0]))):
-            self.psi_qubits = self.psi_qubits / norm_qubits
+        if normalize:
+            # Normalize the qubits.
             norm_qubits = self.psi_qubits * np.conjugate(self.psi_qubits)
-            norm_qubits = np.sqrt(norm_qubits.sum(axis=1))
-            assert np.all(
-                np.isclose(norm_qubits, np.ones(self.psi_qubits.shape[0])))
-            print("Normalizing the qubits states.")
+            norm_qubits = np.sqrt(norm_qubits.sum(axis=1)).reshape(-1, 1)
+            if not np.all(
+                    np.isclose(norm_qubits, np.ones(self.psi_qubits.shape[0]))):
+                self.psi_qubits = self.psi_qubits / norm_qubits
+                norm_qubits = self.psi_qubits * np.conjugate(self.psi_qubits)
+                norm_qubits = np.sqrt(norm_qubits.sum(axis=1))
+                assert np.all(
+                    np.isclose(norm_qubits, np.ones(self.psi_qubits.shape[0])))
+                print("Normalizing the qubits states.")
+
+    def interact(self):
+        self.psi.amplitude = self.psi.amplitude
+
+    def beam_split(self):
+        self.psi.amplitude = self.symmetric_BS(self.N) @ self.psi.amplitude
+    
+    def measure(self):
+        for n in range(self.N+1):
+            mask = self.psi.index.map(lambda x: 1 if x[1] == n else 0)
+            amplitude = mask * self.psi.amplitude
+            probability = amplitude @ np.conjugate(amplitude)
+            print(n, probability)
 
     @staticmethod
     def symmetric_BS(N):
-        phi = np.exp(1j*np.pi/N)
-        return phi
+        phi = np.exp(2*np.pi*1j/N)
+        BS = np.empty((N+1, N+1), dtype=complex)
 
+        for n in range(N+1):
+            for m in range(N+1):
+                if n > 0 and m > 0:
+                    BS[n, m] = phi**((n-1)*(m-1))
+                elif n == m == 0:
+                    BS[n, m] = 1
+                else:
+                    BS[n, m] = 0
 
-A = IFM(N=3)
+        BS = np.kron(np.eye(2**N), BS)
+
+        return BS
+
+    @staticmethod
+    def int_to_binary(k, N):
+        """
+        Convert integer k to a binary string of length N,
+        using '0' and '1' characters.
+        """
+        return bin(k)[2:].zfill(N)[-N:]
+
+A = IFM(N=2)
 A()
+psi = A.psi
+BS = A.symmetric_BS(A.N)
 
-# for k in 
 
-# def foo(i, n, N, phi, P, Z):
-#     """
-#     Probability of ⟨bin_N(i)|
-#     """
-#     def inner_sum(j, phi, N):
-#         return sum(
-#             phi**((n-1)*(m-1))*kappa_prime(m, j, N)
-#             for m in range(1, N+1))
-    
-#     def outer_sum(N, P, Z):
-#         total = sum(
-#             Z[i, j]*inner_sum(j, phi, N)/(P[n]*np.sqrt(N))
-#             for j in range(1, 2**(N-1)-1))
-#         return total*np.conjugate(total)
-        
-    
+#%%
+
